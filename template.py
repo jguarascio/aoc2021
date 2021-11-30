aec718c52c680fa0f8ccbb3780b242580fc44ee6
Joe Guarascio <joe.guarascio@cotiviti.com>
2021-11-30 15:28:40 -0500

    first commit

---
+++ template.py
@@ -0,0 +1,24 @@
+INPUT_FILE = "input.txt"
+
+def get_lines(filename)->List:
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def part1():
+    lines = get_lines(INPUT_FILE)
+
+    return
+
+def part2():
+    lines = get_lines(INPUT_FILE)
+
+    return
+
+def main():
+    part1()
+    part2()
+
+if __name__ == "__main__":
+    main()
